41744d90f16909d19955ea746ca4401f7b34899d
Nayelyyy09 <nayelydoroteo09@gmail.com>
2026-07-20 14:57:54 -0500

    fix: navbar max-width 1400px minimo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -122,7 +122,7 @@ export default function Navbar() {
           : "bg-[#0A461A]/90 backdrop-blur-sm"
           }`}
       >
-        <div className="max-w-7xl mx-auto px-6 lg:px-16 flex items-center justify-between h-28 lg:h-32">
+        <div className="max-w-[1400px] mx-auto px-6 lg:px-16 flex items-center justify-between h-28 lg:h-32">
           {/* Logo */}
           <Link to="/" className="flex items-center gap-3 flex-shrink-0 relative group">
             <img
